b9a3cad89d99b5187fa02ebbb12d385ea9d26b9c
Daniel Voros <danithered@live.com>
2021-03-08 21:50:09 +0100

    fixed output file header
    put commas elsewhere
    	modified:   src/ca.cpp

--- src/ca.cpp
+++ src/ca.cpp
@@ -574,14 +574,14 @@ namespace cadv {
 		}
 
 		//adding header to output
-		output << "time;replicators;";
-		output << "no_par;mean_R_par;mean_length_par;mean_mfe_par;" ;
+		output << "time;replicators";
+		output << ";no_par;mean_R_par;mean_length_par;mean_mfe_par" ;
 		for(int e = 0; e < par_noEA; e++){
-			output << "no_enz" << e << ";mean_R_enz" << e << ";mean_length_enz" << e << ";mean_mfe_enz" << e << ";mean_a_enz" << e << ";" ;
+			output << ";no_enz" << e << ";mean_R_enz" << e << ";mean_length_enz" << e << ";mean_mfe_enz" << e << ";mean_a_enz" << e ;
 		}
 
 		for(int a = 0; a <= par_noEA; a++) {
-			output << "no_A" << a << ";" ;
+			output << ";no_A" << a ;
 		}
 
 		output << std::endl;
