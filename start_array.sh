018f28feeb3fa216f10c610abb2c898bd25768cd
danithered <danithered@live.com>
2021-10-11 22:48:13 +0200

    created start_array.sh
    it is to queue simulations when slurm is absent
    	new file:   doc/Balazs_inicpool_t0_A.png
    	new file:   doc/Balazs_inicpool_t0_Amap.png
    	new file:   doc/Balazs_inicpool_t0_M.png
    	new file:   doc/Balazs_inicpool_t0_R.png
    	new file:   doc/Balazs_inicpool_t0_mfe.png
    	new file:   doc/Balazs_inicpool_t0_types.png
    	new file:   doc/bonuses.png
    	new file:   doc/grid.png
    	new file:   doc/hist_freq_random_strs.png
    	new file:   doc/mfe_0.png
    	new file:   doc/mfe_1M.png
    	new file:   doc/timeplot.png
    	new file:   doc/tr.png
    	new file:   start_array.sh
    	modified:   start_slurmarray.sh

--- start_array.sh
+++ start_array.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-par_ID=starttest
+# general settings
 
+maxnum=4
+par_ID=starttest
 no_repeats=10
 
+#parameter settings
+par_noEA=4
 par_maxtime=50000
-par_ncol=
-par_nrow=
+par_ncol=300
+par_nrow=300
 par_output_interval=
 par_save_interval=1000
 par_seed=
-#par_seed_plus=
-par_str_pool=
+par_str_pool=IN/src/mappingA4_80.txt
 par_outdir=
 par_output_filename=
 par_savedir=
@@ -33,13 +36,15 @@ par_Emin=
 par_Nmet=8
 par_Nrep=8
 
+#other settings
 
 direct="IN"
 outdirect="OUT"
 savedirect="save"
-log="slurmjobs.txt"
+log="arrayjobs.txt"
 
 
+#creating stuff
 if [ ! -d  $outdirect ]; then
 	mkdir $outdirect
 fi
@@ -48,11 +53,6 @@ fi
 if [ ! -d  $direct ]; then
 	mkdir IN
 fi
-
-# if [ -e $direct/$file ]; then
-# 	cp $direct/$file $direct/$file$(date +"%T")
-# 	rm $direct/$file
-# fi 
 
 #find a jobname
 try=1
@@ -77,6 +77,7 @@ touch $direct/$file
 
 #creating parameter file
 ./src/paramfile_gen.R $(printf "%s%s" "--par_Nrep" $(printf ",%s" "${par_Nrep[@]}")) $(printf "%s%s" "--par_Nmet" $(printf ",%s" "${par_Nmet[@]}")) $(printf "%s%s" "--par_Emin" $(printf ",%s" "${par_Emin[@]}")) $(printf "%s%s" "--par_c" $(printf ",%s" "${par_c[@]}")) $(printf "%s%s" "--par_b2" $(printf ",%s" "${par_b2[@]}")) $(printf "%s%s" "--par_b1" $(printf ",%s" "${par_b1[@]}")) $(printf "%s%s" "--par_g" $(printf ",%s" "${par_g[@]}")) $(printf "%s%s" "--par_deletion" $(printf ",%s" "${par_deletion[@]}")) $(printf "%s%s" "--par_insertion" $(printf ",%s" "${par_insertion[@]}")) $(printf "%s%s" "--par_substitution" $(printf ",%s" "${par_substitution[@]}")) $(printf "%s%s" "--par_claimEmpty" $(printf ",%s" "${par_claimEmpty[@]}")) $(printf "%s%s" "--par_save_interval" $(printf ",%s" "${par_sigma[@]}")) $(printf "%s%s" "--par_ll" $(printf ",%s" "${par_ll[@]}")) $(printf "%s%s" "--par_diffusion_rate" $(printf ",%s" "${par_diffusion_rate[@]}")) $(printf "%s%s" "--par_init_grid" $(printf ",%s" "${par_init_grid[@]}")) $(printf "%s%s" "--par_seed_file" $(printf ",%s" "${par_seed_file[@]}")) $(printf "%s%s" "--par_load" $(printf ",%s" "${par_load[@]}")) $(printf "%s%s" "--par_savedir" $(printf ",%s" "${par_savedir[@]}")) $(printf "%s%s" "--par_output_filename" $(printf ",%s" "${par_output_filename[@]}")) $(printf "%s%s" "--par_outdir" $(printf ",%s" "${par_outdir[@]}")) $(printf "%s%s" "--par_str_pool" $(printf ",%s" "${par_str_pool[@]}")) $(printf "%s%s" "--par_seed" $(printf ",%s" "${par_seed[@]}")) $(printf "%s%s" "--par_save_interval" $(printf ",%s" "${par_save_interval[@]}")) $(printf "%s%s" "--par_output_interval" $(printf ",%s" "${par_output_interval[@]}")) $(printf "%s%s" "--par_nrow" $(printf ",%s" "${par_nrow[@]}")) $(printf "%s%s" "--par_maxtime" $(printf ",%s" "${par_maxtime[@]}")) $(printf "%s%s" "--par_ncol" $(printf ",%s" "${par_ncol[@]}") ) >> $direct/$file
+
 if (( $(wc -w < $direct/$file ) > 0 ))
 then 
 	echo >> $direct/$file
@@ -110,104 +111,42 @@ fi
 #creating job description
 touch $outdirect/output_$jobname
 
+#number of runs
 num=$(wc -l < $direct/$file)
 
-touch $jobname.sh
-echo "#!/bin/bash" >>$jobname.sh
-echo "#SBATCH -A eletered" >>$jobname.sh
-echo "#SBATCH --job-name="$jobname >>$jobname.sh
-echo "#SBATCH --time=12-12:00:00" >>$jobname.sh
-echo "#SBATCH --array=1-"$num >>$jobname.sh
-echo "#SBATCH --no-requeue" >>$jobname.sh
-echo "#SBATCH --mail-type=ALL" >>$jobname.sh
-echo "#SBATCH --mail-user=danithered@live.com" >>$jobname.sh
-echo >>$jobname.sh 
-echo 'srun ./simulation $(sed "${SLURM_ARRAY_TASK_ID}q;d"' $direct/$file')' --par_seed_plus '$SLURM_ARRAY_TASK_ID' --par_ID $jobname'_$SLURM_ARRAY_TASK_ID' '>>' $outdirect/output_$jobname '2>&1' >>$jobname.sh
-
-sudo chmod +x $jobname.sh
-
-# submit job
-sbatch ./$jobname.sh
-
-# remove jobdescription file
-rm $jobname.sh
+#starting jobs
+for ((i=1; i <= ${num}; i+=1))
+do
+	src/creat_inic_matrix.sh 4 300 300 80 > IN/mapping_$jobname'_'${i}.txt
+	#echo $(sed "${i}q;d" $direct/$file) --par_seed_plus $i --par_ID $jobname'_'${i} --par_load IN/mapping_$jobname'_'${i}.txt '>>' $outdirect/output_$jobname '2>&1'
+	nohup ./simulation $(sed "${i}q;d" $direct/$file) --par_seed_plus $i --par_ID $jobname'_'${i} --par_load IN/mapping_$jobname'_'${i}.txt '>>' $outdirect/output_$jobname '2>&1'
+	pid[ $(( i - 1)) ]=$!
+	
+	#echo pid no $(( i - 2)) is ${pid[ $(( i - 2)) ]}
+	#echo pids are ${pid[@]}
+	#echo pids are ${pid[@]} numrun is $numrun maxnum is $maxnum
+		
+	#count how many of the processes still running - if a lot, than stall
+	numrun=$(( maxnum + 1 ))
+	while [ "$numrun" -ge "$maxnum" ]
+	do
+		#echo pid indexes are ${!pid[@]} numrun is $numrun maxnum is $maxnum
+		sleep 1
+		numrun=0
+		for c in ${!pid[@]} 
+		do
+			#echo c is ${c}
+			if kill -0 ${pid[c]}; then
+				numrun=$(( numrun + 1 ))
+			fi
+		done
+		#echo numrun is $numrun
+	done
+done
 
 # writing to log
 echo $jobname array job started with $num jobs at $(date +"%D %T"). Paramfile: $file, output file: $outdirect/output_$jobname >> $outdirect/$log
 
-# <$indirect/$file
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# #for i in {0..2}
-# for m in ${met_neigh_meret[@]}
-# do
-#   for k in ${kmax[@]}
-#   do
-#     for b in ${tradeoffEE[@]}
-#     do
-#       for d in ${diffuzioGyak[@]}
-#       do
-#         for e in ${enzakt_num[@]}
-#         do
-#           for r in ${repl_neigh_meret[@]}
-#           do
-#             for i in {1..1}}
-#             do
-# 	      #echo 10 10 0 0 0 $(($i+5)) >> file1
-# 	      echo $ncol $nrow ${e} ${m} ${r} $ciklusszam $mintavetel_gyak $cellamintavetel_gyak $alapfeltoltes $emax ${b} $tradeoffEK $kmin ${k} $phalal $claimEmpty $sd $spec_limit ${d} $pMutacio $mentes >> $direct/$file
-#             done
-#           done
-#         done
-#       done
-#     done
-#   done
-# done
-# #$(($diffuzioGyak+$i))
-
-
-
-
-
-
-
-#a=(1 2)
-#b=""
-
-#if[ -z "$a" ]
-#then 
-# 	echo "empty"
-# else
-# 	echo "not empty"
-# fi
-# 
-# if [ -z "$a" ]
-# then 
-# 	for x in ${a[@]}
-# 	do 
-# 		echo $x
-# 	done
-# fi
-# 
-# 
-# array=("1 2" "A B")
-# 
-# for list in "${array[@]}"
-# do
-#     for item in $list
-#     do
-#         echo $list : $item
-#     done
-# done
